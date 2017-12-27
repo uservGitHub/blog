f6a540723ba398eb4d759c5564799d1637f2c12d
hihl <348267823@qq.com>
2017-12-27 19:14:16 +0800

    添加配置

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,3 +2,4 @@
  * Created by Zhengfeng.Yao on 2017/12/26.
  */
 import assets from './website/assets.json';
+console.log(assets);
--- src/website/index.js
+++ src/website/index.js
@@ -1,3 +1,4 @@
 /**
  * Created by Zhengfeng.Yao on 2017/12/26.
  */
+const test = () => {};
