260763bc234c50d19d2a0b024895122bcf523700
hihl <348267823@qq.com>
2018-01-08 15:04:37 +0800

    添加代码高亮和收缩展开

--- src/server/renderView.ts
+++ src/server/renderView.ts
@@ -9,7 +9,7 @@ export default function renderView(page: string, config: any) {
       <title>hihl</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-      ${page === 'articles' ? '<link rel="stylesheet" href="/highlight/9.12.0/github.min.css">' : ''}
+      ${page === 'articles' ? '<link rel="stylesheet" href="/highlight/9.12.0/monokai.min.css">' : ''}
       <link rel="stylesheet" href="${assets[page].css}">
       <script> window.__CONFIG__ = ${JSON.stringify(others)} </script>
     </head>
--- src/site/pages/articles/CodeBlock.tsx
+++ src/site/pages/articles/CodeBlock.tsx
@@ -23,23 +23,29 @@ export default class CodeBlock extends React.Component<{ value: string, language
   render() {
     const { value, language } = this.props;
     const { collapse } = this.state;
+    const block = [
+      <div key="button" style={{ padding: 10, textAlign: 'right' }}>
+        <a
+          style={{ cursor: 'pointer' }}
+          onClick={() => this.setState({ collapse: !collapse })}
+        >
+          { collapse ? '展开' : '收缩' }
+        </a>
+      </div>,
+      <code
+        key="code"
+        ref={(el:any) => this.codeEl = el}
+        style={{ height: collapse ? 80 : '100%' }}
+        className={language}>
+        {value}
+      </code>
+    ];
 
     return (
       <pre>
-        <div style={{ padding: 10, textAlign: 'right' }}>
-          <a
-            style={{ cursor: 'pointer' }}
-            onClick={() => this.setState({ collapse: !collapse })}
-          >
-          { collapse ? '展开' : '收缩' }
-        </a>
-        </div>
-        <code
-          ref={(el:any) => this.codeEl = el}
-          style={{ height: collapse ? 80 : '100%' }}
-          className={language}>
-          {value}
-        </code>
+        {
+          collapse ? block : block.reverse()
+        }
       </pre>
     );
   }
